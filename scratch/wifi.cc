c8aabe2e92e4cba1f3eca561967402ffb82240eb
YouhuiY <478263354@qq.com>
2017-03-20 06:08:51 -0700

    change the way to store information and change the output from integer to double

--- scratch/wifi.cc
+++ scratch/wifi.cc
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 //my includes
 #include "/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/includes/aux.hh"
@@ -21,35 +22,54 @@ NS_LOG_COMPONENT_DEFINE ("before_change");
 
 struct results
 {
-  double hits = 0;  
+   double times = 0;
+   uint64_t packets = 0;  
 };
-struct results result;
+struct results DCR;
+struct results DTR;
 
 int v;
 std::ofstream outData;
 // define the trace function
-void DC_Delay (struct results *result, Time oldValue, Time newValue)
+void DC_Delay (struct results *DCR, Time oldValue, Time newValue)
 {
 
   uint64_t temp_new = newValue.GetMilliSeconds ();
   //outData.open("/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/scratch/delay_0.dat", std::ios::app);
-  DC_DelayTime += temp_new;
-  nDCPacket ++;
+  DCR->times = DCR->times * (double)DCR->packets + (double)temp_new;
+  DCR->packets ++;
+  DCR->times = DCR->times / (double)DCR->packets;
+  /*std::ofstream outData;
+      outData.open("/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/scratch/wifi-1.dat", std::ios::app);
+      if (!outData)
+      {
+        std::cout <<"can not open the file"<< std::endl;
+      }
+      else
+      {
+        outData  <<temp_new <<" " << DCR->times <<" " << DCR-> packets<<std::endl;
+        outData.close();
+      }
+  //std::cout 
+  system("PAUSE");*/
   
 }
 
-void DT_Delay (struct results *result, Time oldValue, Time newValue)
+void DT_Delay (struct results *DTR, Time oldValue, Time newValue)
 {
 
   uint64_t temp_new = newValue.GetMilliSeconds ();
-  DT_DelayTime += temp_new;
-  nDTPacket ++;
+  //outData.open("/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/scratch/delay_0.dat", std::ios::app);
+  DTR->times = DTR->times *(double) DTR->packets + (double)temp_new;
+  DTR->packets ++;
+  DTR->times = DTR->times / (double)DTR->packets;
+  //std::cout <<temp_new <<std::endl;
   
 }
  
 int main (int argc, char *argv[])
 {
-  double simulationTime = 60; //seconds
+  double simulationTime = 20; //seconds
   int mcs = 9;
   int channelWidth = 160;
   int gi = 1; //Guard Interval
@@ -126,180 +146,182 @@ int main (int argc, char *argv[])
 
   for(v = 1; v <= DC_nsta; v+=2)
   {
-
-    NodeContainer DCStaNodes;
-    DCStaNodes.Create (v);
-    NodeContainer DTStaNodes;
-    DTStaNodes.Create (v);
-    NodeContainer wifiApNode;
-    wifiApNode.Create (1);
-    std::cout << "MCS value: " << mcs << "Channel width: " << channelWidth << "short GI: " << gi << "number of delay-critical devices: " << v << "number of delay-tolerant devices: " << v << std::endl;
-
-
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
-    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
-    phy.SetChannel (channel.Create ());
-    
-      // Set guard interval
-    phy.Set ("ShortGuardEnabled", BooleanValue (gi));
-
-    WifiHelper wifi;
-    wifi.SetStandard (WIFI_PHY_STANDARD_80211ac);
-    WifiMacHelper mac;
-                  
-    std::ostringstream oss;
-    oss << "VhtMcs" << mcs;
-    wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager","DataMode", StringValue (oss.str ()),
-                                  "ControlMode", StringValue (oss.str ()));
-    
-
-    Ssid ssid = Ssid ("ns3-80211ac");
-
-    mac.SetType ("ns3::StaWifiMac",
-                "Ssid", SsidValue (ssid));
-
-    NetDeviceContainer DC_staDevices;
-    DC_staDevices = wifi.Install (phy, mac,DCStaNodes);
-    NetDeviceContainer DT_staDevices;
-    DT_staDevices = wifi.Install (phy, mac,DTStaNodes);
-
-    mac.SetType ("ns3::ApWifiMac",
-                "Ssid", SsidValue (ssid));
-
-    NetDeviceContainer apDevice;
-    apDevice = wifi.Install (phy, mac, wifiApNode);
-
-
-    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-      
-    MobilityHelper mobility;
-    positionAlloc->Add (Vector (0.0, 0.0, 0.0));
-    positionAlloc->Add (Vector (distance, 0.0, 0.0));
-    positionAlloc->Add (Vector (distance, 0.0, 0.0));
-    
-
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-
-    mobility.Install (wifiApNode);
-    mobility.Install (DCStaNodes);
-    mobility.Install (DTStaNodes);
-
-    InternetStackHelper stack;
-    stack.Install (wifiApNode);
-    stack.Install (DCStaNodes);
-    stack.Install (DTStaNodes);
-
-    Ipv4AddressHelper address;
-    address.SetBase ("192.168.0.0", "255.255.0.0");
-    
-    Ipv4InterfaceContainer apNodeInterface;
-    Ipv4InterfaceContainer DCstaNodeInterface;
-    Ipv4InterfaceContainer DTstaNodeInterface;
-
-    apNodeInterface.Add( address.Assign (apDevice));
-    //uint32_t  apint = apNodeInterface.GetAddress(0)//.ConvertTo().m_data();
-   // std::string st = apNodeInterface.GetAddress(0).toString();
-   // std::cout <<"the ap's address is: "<<st<< std::endl;
-   
-    int idx = 0;
-    for (int s = 0; s < v; s++)
-    { 
-      DCstaNodeInterface.Add( address.Assign (DC_staDevices.Get (idx)));
-      idx ++;
-    }
-      
-    idx = 0;
-    for (int s = 0; s < v; s++)
+   for( int u = 0; u < 3; u ++)
     {
-      DTstaNodeInterface.Add( address.Assign (DT_staDevices.Get (idx)));
-      idx ++;
-    }
-//uint32_t ni = apNodeInterface.GetAddress(0).ptr;
-/*  ******************** test *********************
 
-std::cout <<apNodeInterface.GetAddress(0).m_address<< std::endl;
-std::cout <<apNodeInterface.GetAddress(0)<< std::endl;
-std::cout <<DCstaNodeInterface.GetAddress(0).m_address<< std::endl;
-std::cout <<DCstaNodeInterface.GetAddress(0)<< std::endl;
-
-*/
-   
-   ap_one = apNodeInterface.GetAddress(0).m_address;
-   DC_down = DCstaNodeInterface.GetAddress(0).m_address;
-   DC_up = DCstaNodeInterface.GetAddress(v - 1).m_address;
-   DT_down = DTstaNodeInterface.GetAddress(0).m_address;
-   DT_up = DTstaNodeInterface.GetAddress(v - 1).m_address;
-   //std::cout <<DT_up<< std::endl;
+	    NodeContainer DCStaNodes;
+	    DCStaNodes.Create (v);
+	    NodeContainer DTStaNodes;
+	    DTStaNodes.Create (v);
+	    NodeContainer wifiApNode;
+	    wifiApNode.Create (1);
+	    std::cout << "MCS value: " << mcs << "Channel width: " << channelWidth << "short GI: " << gi << "number of delay-critical devices: " << v << "number of delay-tolerant devices: " << v << std::endl;
 
 
-     // ApplicationContainer serverApp, DC_clientApps, DT_clientApps;
+	    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+	    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+	    phy.SetChannel (channel.Create ());
+	    
+	      // Set guard interval
+	    phy.Set ("ShortGuardEnabled", BooleanValue (gi));
 
-    int port = 9000;
-    idx = 0;
+	    WifiHelper wifi;
+	    wifi.SetStandard (WIFI_PHY_STANDARD_80211ac);
+	    WifiMacHelper mac;
+		          
+	    std::ostringstream oss;
+	    oss << "VhtMcs" << mcs;
+	    wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager","DataMode", StringValue (oss.str ()),
+		                          "ControlMode", StringValue (oss.str ()));
+	    
 
-    ApplicationContainer serverApp;
+	    Ssid ssid = Ssid ("ns3-80211ac");
 
-    UdpServerHelper myServer (9);
-    serverApp = myServer.Install (wifiApNode.Get (0));
-    serverApp.Start (Seconds (0.0));
-    serverApp.Stop (Seconds (simulationTime + 1));
+	    mac.SetType ("ns3::StaWifiMac",
+		        "Ssid", SsidValue (ssid));
 
-    UdpClientHelper myClient (apNodeInterface.GetAddress (0), 9);
-    myClient.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
-    myClient.SetAttribute ("Interval", TimeValue (Time ("0.00001"))); //packets/s
-    myClient.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+	    NetDeviceContainer DC_staDevices;
+	    DC_staDevices = wifi.Install (phy, mac,DCStaNodes);
+	    NetDeviceContainer DT_staDevices;
+	    DT_staDevices = wifi.Install (phy, mac,DTStaNodes);
 
-    ApplicationContainer clientApps;
+	    mac.SetType ("ns3::ApWifiMac",
+		        "Ssid", SsidValue (ssid));
 
-    clientApps.Add (myClient.Install (DCStaNodes));
-    clientApps.Add (myClient.Install (DTStaNodes));
-    clientApps.Start (Seconds (1.0));
-    clientApps.Stop (Seconds (simulationTime + 1));
-      
+	    NetDeviceContainer apDevice;
+	    apDevice = wifi.Install (phy, mac, wifiApNode);
 
-  
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-    // tracer callback function
-    Ptr<UdpServer> server = DynamicCast<UdpServer> (serverApp.Get (0));
-    server->TraceConnectWithoutContext ("Delay_DC", MakeBoundCallback (&DC_Delay, &result));
-    
-    server->TraceConnectWithoutContext ("Delay_DT", MakeBoundCallback (&DT_Delay, &result));
 
-    
-    //Ptr<UdpServer> server = DynamicCast<UdpServer> (serverApp.Get(0));
-    //server->TraceConnectWithoutContext ("Delay", MakeBoundCallback (&TraceArray));
-    for( int u = 0; u < 1; u ++)
-    {
-      Simulator::Stop (Seconds (simulationTime + 1));
-      Simulator::Run ();
-      Simulator::Destroy ();
-    }
+	    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+	      
+	    MobilityHelper mobility;
+	    positionAlloc->Add (Vector (0.0, 0.0, 0.0));
+	    positionAlloc->Add (Vector (distance, 0.0, 0.0));
+	    positionAlloc->Add (Vector (distance, 0.0, 0.0));
+	    
 
-    double T = 0.0;
-    uint32_t totalPacketsThrough = DynamicCast<UdpServer> (serverApp.Get (0))->GetReceived ();
-    //std::cout<<totalPacketsThrough<<std::endl;
-    uint64_t m = totalPacketsThrough*8*payloadSize;
-    //std::cout<<m<<std::endl;
-    uint32_t n = simulationTime*1000000.0; 
-    //std::cout<<n<<std::endl;
-    T = m / n / 20.0; //Mbit/s
+	    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
 
-    std::ofstream outData;
-    outData.open("/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/scratch/wifi.dat", std::ios::app);
-    if (!outData)
-    {
-      std::cout <<"can not open the file"<< std::endl;
-    }
-    else
-    {
-      outData <<"number of each devices: " << v << "total throughput: " << T << "DC delay: " << DC_DelayTime/nDCPacket << "DT delay: " << DT_DelayTime/nDTPacket << std::endl;
-      outData.close();
-    }
+	    mobility.Install (wifiApNode);
+	    mobility.Install (DCStaNodes);
+	    mobility.Install (DTStaNodes);
 
-    DT_DelayTime = 0;
-    DC_DelayTime = 0;
-    nDTPacket = 0;
-    nDCPacket = 0;
+	    InternetStackHelper stack;
+	    stack.Install (wifiApNode);
+	    stack.Install (DCStaNodes);
+	    stack.Install (DTStaNodes);
+
+	    Ipv4AddressHelper address;
+	    address.SetBase ("192.168.0.0", "255.255.0.0");
+	    
+	    Ipv4InterfaceContainer apNodeInterface;
+	    Ipv4InterfaceContainer DCstaNodeInterface;
+	    Ipv4InterfaceContainer DTstaNodeInterface;
+
+	    apNodeInterface.Add( address.Assign (apDevice));
+	    //uint32_t  apint = apNodeInterface.GetAddress(0)//.ConvertTo().m_data();
+	   // std::string st = apNodeInterface.GetAddress(0).toString();
+	   // std::cout <<"the ap's address is: "<<st<< std::endl;
+	   
+	    int idx = 0;
+	    for (int s = 0; s < v; s++)
+	    { 
+	      DCstaNodeInterface.Add( address.Assign (DC_staDevices.Get (idx)));
+	      idx ++;
+	    }
+	      
+	    idx = 0;
+	    for (int s = 0; s < v; s++)
+	    {
+	      DTstaNodeInterface.Add( address.Assign (DT_staDevices.Get (idx)));
+	      idx ++;
+	    }
+	//uint32_t ni = apNodeInterface.GetAddress(0).ptr;
+	/*  ******************** test *********************
+
+	std::cout <<apNodeInterface.GetAddress(0).m_address<< std::endl;
+	std::cout <<apNodeInterface.GetAddress(0)<< std::endl;
+	std::cout <<DCstaNodeInterface.GetAddress(0).m_address<< std::endl;
+	std::cout <<DCstaNodeInterface.GetAddress(0)<< std::endl;
+
+	*/
+	   
+	   ap_one = apNodeInterface.GetAddress(0).m_address;
+	   DC_down = DCstaNodeInterface.GetAddress(0).m_address;
+	   DC_up = DCstaNodeInterface.GetAddress(v - 1).m_address;
+	   DT_down = DTstaNodeInterface.GetAddress(0).m_address;
+	   DT_up = DTstaNodeInterface.GetAddress(v - 1).m_address;
+	   //std::cout <<DT_up<< std::endl;
+
+
+	     // ApplicationContainer serverApp, DC_clientApps, DT_clientApps;
+
+	    int port = 9000;
+	    idx = 0;
+
+	    ApplicationContainer serverApp;
+
+	    UdpServerHelper myServer (9);
+	    serverApp = myServer.Install (wifiApNode.Get (0));
+	    serverApp.Start (Seconds (0.0));
+	    serverApp.Stop (Seconds (simulationTime + 1));
+
+	    UdpClientHelper myClient (apNodeInterface.GetAddress (0), 9);
+	    myClient.SetAttribute ("MaxPackets", UintegerValue (4294967295u));
+	    myClient.SetAttribute ("Interval", TimeValue (Time ("0.00001"))); //packets/s
+	    myClient.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+
+	    ApplicationContainer clientApps;
+
+	    clientApps.Add (myClient.Install (DCStaNodes));
+	    clientApps.Add (myClient.Install (DTStaNodes));
+	    clientApps.Start (Seconds (1.0));
+	    clientApps.Stop (Seconds (simulationTime + 1));
+	      
+
+	  
+	    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+	    // tracer callback function
+	    Ptr<UdpServer> server = DynamicCast<UdpServer> (serverApp.Get (0));
+	    server->TraceConnectWithoutContext ("Delay_DC", MakeBoundCallback (&DC_Delay, &DCR));
+	    
+	    server->TraceConnectWithoutContext ("Delay_DT", MakeBoundCallback (&DT_Delay, &DTR));
+
+	    
+	    //Ptr<UdpServer> server = DynamicCast<UdpServer> (serverApp.Get(0));
+	    //server->TraceConnectWithoutContext ("Delay", MakeBoundCallback (&TraceArray));
+	    
+	      Simulator::Stop (Seconds (simulationTime + 1));
+	      Simulator::Run ();
+	      Simulator::Destroy ();
+	    
+
+	    double T = 0.0;
+	    uint32_t totalPacketsThrough = DynamicCast<UdpServer> (serverApp.Get (0))->GetReceived ();
+	    //std::cout<<totalPacketsThrough<<std::endl;
+	    uint64_t m = totalPacketsThrough*8*payloadSize;
+	    //std::cout<<m<<std::endl;
+	    uint32_t n = simulationTime*1000000.0; 
+	    //std::cout<<n<<std::endl;
+	    T = (double)m / (double)n; //Mbit/s
+
+	    std::ofstream outData;
+	    outData.open("/home/youhui/Downloads/ns-allinone-3.26/ns-3.26/scratch/wifi.dat", std::ios::app);
+	    if (!outData)
+	    {
+	      std::cout <<"can not open the file"<< std::endl;
+	    }
+	    else
+	    {
+	      outData  << v << " " << T << " " << DCR.times << " " <<DTR.times << std::endl;
+	      outData.close();
+	    }
+
+	    DCR.times = 0;
+	    DCR.packets = 0;
+	    DTR.times = 0;
+	    DTR.packets = 0;
+  }
 
 }
     
